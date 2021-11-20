540d3af8d84c4a43c3e740f69e1074dcc666d1af
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-11-19 22:33:45 -0600

    coded logic for pw generation; set character selections to checked by default

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwRange = document.querySelector("#pw-range");
 
 function generatePassword() {
+  // get value from pwLength slider input
+  const pwLength = pwRange.value; 
+  charSet = setCharacterArray();
+  var pwStr = "";
   
+  for(var i = 0; i < pwLength; i++) {
+    // pull random character from charSet and add to new string
+    const random = Math.random() * charSet.length;
+    const floored = Math.floor(random);
+    pwStr += charSet[floored];
+  }
+  return pwStr;
+}
+
+// compile array of characters to use for password based on user input
+function setCharacterArray() {
+  // arrays of characters to use in password
+  const uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  const lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+  const numbers = ["0123456789"];
+  const symbols = [" !\"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~"]; // special chars includes " " space
+  // empty array for chosen characters
+  var chosenChars = [];
+  // get selections from inputs
+  document.getElementById("uppercase").checked ? chosenChars += uppercase : void 0;
+  document.getElementById("lowercase").checked ? chosenChars += lowercase : void 0;
+  document.getElementById("numbers").checked ? chosenChars += numbers : void 0;
+  document.getElementById("special-char").checked ? chosenChars += symbols : void 0;
+
+  // verify there are characters to use for the password and return
+  if (chosenChars.length > 0) {
+    return chosenChars
+   } else {
+      alert("Please select at least one type of character to use in your password!");
+   }
 }
 
 // Write password to the #password input
@@ -13,6 +48,26 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// // Get value for password length slider and add to slider as bubble
+
+// function setPwValue(range, pwValueBubble) {
+//   const value = pwRange.value;
+//   const min = pwRange.min;
+//   const max = pwRange.max;
+//   const newValue = Number(((value - min) * 100) / (max - min));
+//   pwValueBubble.innerhtml = value
+
+//   pwValueBubble.style.left = newValue = "%";
+// }
+
+// Add event listener to pw-length range input slider
+// pwRange.addEventListener("input", () => {
+//   pwValueBubble.innerhtml = rangel.value;
+// });
+// pwRange.addEventListener("input", () => {
+//   setPwValue(pwRange, pwValueBubble);
+// });
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
